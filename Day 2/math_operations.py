215799fa5ef78d04d8d0149443f2c1dea80366df
Pankaj biradar <pankaj28988@gmail.com>
2024-01-05 22:31:36 +0530

    Add module file

---
+++ Day 2/math_operations.py
@@ -0,0 +1,21 @@
+def add_operation(a, b):
+    return a + b
+
+def subtract_operation(a, b):
+    return a - b
+
+def multiply_operation(a, b):
+    return a * b
+
+def divide_operation(a, b):
+    return a / b
+
+def modulo_operation(a, b):
+    return a % b
+
+def power_operation(a, b):
+    return a ** b
+
+def floor_division_operation(a, b):
+    return a // b
+
