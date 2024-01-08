29cdb35a635eb18d764f380ba4ffff205a57a0bf
Pankaj biradar <pankaj28988@gmail.com>
2024-01-08 22:49:11 +0530

    Add day3 files

---
+++ Day 3/day3.py
@@ -0,0 +1,9 @@
+file = open("Day 3/raw_data.txt", "r")
+file_iter  = iter(file)
+
+print(next(file_iter))
+print(next(file_iter))
+print(next(file_iter))
+print(next(file_iter))
+print(next(file_iter))
+
